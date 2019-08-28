f791e56ee6fb0281633d272c3da96b48385fa03b
Derek <10873409@my.uvu.edu>
2019-08-28 17:10:12 -0600

    unity repo fixed

---
+++ New2dPlatformer/Assets/Scripts/Assignments/Variables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+   string name = "Derek";
+   int age = 23;
+   float height = 6.2f;
+   bool married = false;
+
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("First Name: " + name);
+        Debug.Log("Age: " + age);
+        Debug.Log("Height: " + height);
+        Debug.Log("Marital Status: " + married);
+    }
+}
