7e3bbea1fbc9af0f32e7d3707b216418534b31a1
Derek <10873409@my.uvu.edu>
2019-09-05 14:40:22 -0600

    Added Homework Functions

--- New2dPlatformer/Assets/Scripts/Assignments/Functions.cs
+++ New2dPlatformer/Assets/Scripts/Assignments/Functions.cs
@@ -11,6 +11,8 @@ public class Functions : MonoBehaviour
     int num1;
     int num2;
     int total;
+    public int firstnum = 0;
+    public int secondnum = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,23 +23,76 @@ public class Functions : MonoBehaviour
         // age = 90;
 
         // Customer(firstName, lastName, age);
+        int num1 = 2;
+        int num2 = 3;
+
         CalcAdd(11,23);
 
+        CalcSub(55,5);
+
+        CalcMult(num1, num2);
+
+        CalcDiv(6,2);
+
+        CalcMathAdd(firstnum, secondnum);
+        
+        CalcMathSub(firstnum, secondnum);
     }
 
     int CalcAdd (int n1, int n2)
     {
-        Debug.Log("First Number = " + n1 + " Second Number = " + n2);
+        Debug.Log("adding First Number: " + n1 + " and Second Number: " + n2);
         total = n1 + n2;
 
         Debug.Log(total);
 
         return total; 
-
-        
     }
    
-    
+    int CalcSub (int num1, int num2)
+    {
+        Debug.Log("subtracting First Number: " + num1 + " and Second Number: " + num2);
+        total = num1 - num2;
+        Debug.Log(total);
+
+        return total;
+    }
+
+    int CalcMult (int one, int two)
+    {
+        Debug.Log("multiplying First Number: " + one + " and Second Number: " + two);
+        total = one * two;
+        Debug.Log(total);
+        return total;
+    }
+
+    int CalcDiv (int num1, int num2)
+    {
+        
+
+        Debug.Log("dividing First Number: " + num1 + " by Second Number: " + num2);
+        total = num1 / num2;
+        Debug.Log(total);
+        return total;
+    }
+
+    int CalcMathAdd (int lead, int follow)
+    {
+        Debug.Log("Adding your numbers: " + firstnum + "+" + secondnum);  
+        total = firstnum + secondnum;
+        Debug.Log(total);
+        return total;
+    } 
+
+    int CalcMathSub (int lead, int follow)
+    {
+        Debug.Log("Subtracting your numbers: " + firstnum + "-" + secondnum);
+        total= firstnum - secondnum;
+        Debug.Log(total);
+        return total;
+    }
+
+
     // void Customer(string fName, string lName, int a)
     // {
     //     Debug.Log(fName + " " + lName + " " + a);
