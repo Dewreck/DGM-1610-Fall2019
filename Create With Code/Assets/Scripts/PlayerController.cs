84d3e2ceb853beb6d8776930b8821aefd2daee78
Derek <10873409@my.uvu.edu>
2019-09-16 17:06:37 -0600

    edited car data AND added challenge 1 files

--- Create With Code/Assets/Scripts/PlayerController.cs
+++ Create With Code/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //these are pivate variables
-    private float speed = 15.0f;
-    private float turnSpeed = 30.0f;
+    private float speed = 25.0f;
+    private float turnSpeed = 40.0f;
     private float horizontalInput;
     private float forwardInput;
     
